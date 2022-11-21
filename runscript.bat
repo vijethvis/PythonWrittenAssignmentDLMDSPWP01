c45f3c8490db1186befab5b6dc5f0c17e03b1ef9
vijethvis <vijeth79@gmail.com>
2022-11-21 20:16:36 +0530

    adding batch and shell scripts to install virtual envs

---
+++ runscript.bat
@@ -0,0 +1,3 @@
+python -m venv vename
+call vename\Scripts\activate.bat
+call vename\Scripts\pip.exe install -r requirement.txt
---
+++ runscript.sh
@@ -0,0 +1,4 @@
+python -m venv vename
+source .\vename\bin\activate
+.\vename\bin\pip install --upgrade pip
+.\vename\bin\pip install -r requirement.txt
